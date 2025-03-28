f797bd98fa2719bb99d8a57c5f8e677c17fe9805
Halil <halil128kerem123@gmail.com>
2025-03-28 18:15:39 +0300

    Initial Commit

---
+++ KisilerUygulamasi/KisilerUygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KisilerUygulamasiApp.swift
+//  KisilerUygulamasi
+//
+//  Created by Halil Keremoğlu on 28.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct KisilerUygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
